e9ad9b6328c109827a95238bf5cee27bce3aa744
Sebastian Flores <flores7029@gmail.com>
2025-03-17 22:28:12 -0400

    adds fonts to my project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        impact: ['Impact', 'sans-serif'],
+        Impacted: ['Impacted', 'sans-serif'],
+        arial: ['Arial', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
