0c9f98b4d5ed12e9da0e30a5df1dfd2b904cf34d
Sebastian Flores <flores7029@gmail.com>
2025-09-07 23:28:23 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer({
 }: Props) {
     return(
         <footer>
-            <p className={`mx-7 mb-5 ${screen === 'projects' ? 'xl:mb-0' : ''} text-sm text-[#B2B2B2]`}>
+            <p className={`mx-7 my-5 ${screen === 'projects' ? 'xl:mb-0' : ''} text-sm text-[#B2B2B2]`}>
                 Built with <span className="text-[#DFDFDF]">Next.js</span> and <span className="text-[#DFDFDF]">Tailwind CSS</span>. Deployed with <span className="text-[#DFDFDF]">Vercel</span>.
             </p>
         </footer>
