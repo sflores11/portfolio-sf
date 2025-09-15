85bdc9c0fc8432482ffe185f9e66f11a1394e6ad
Sebastian Flores <flores7029@gmail.com>
2025-09-15 19:46:48 -0400

    fixes portrait about page

---
+++ src/components/AboutDesc.tsx
@@ -0,0 +1,16 @@
+export default function AboutDesc() {
+    return (
+        <>
+            <h2 id="about-description" className="sr-only">
+                About description
+            </h2>
+            <p className="font-arial text-[#DFDFDF] mr-3 text-2xl sm:text-base md:text-lg lg:text-[22px] xl:text-2xl 2xl:text-3xl text-[32px] leading-snug">
+                I'm fueled by my desire to create lasting solutions that go beyond an immediate fix. This has allowed me to balance business expectations and complex technological requirements to create practical and impactful solutions.
+                I enjoy building end-to-end full-stack solutions, from database design and backend APIs to intuitive front-end user experiences.
+                <br/>
+                <br/>
+                When I am not developing, you can find me admiring architecture, watching sports, or seeing the world through a viewfinder.
+            </p>
+        </>
+    )
+}
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NavBar from "../components/NavBar";
 import BetterNav from "../components/BetterNav";
 import Footer from "../components/Footer";
+import AboutDesc from "../components/AboutDesc";
 
 export default function About() {
     const [open, setOpen] = useState(false);
@@ -16,18 +17,9 @@ export default function About() {
             </header>
             <main className="flex-grow">
                 {/* desktop */}
-                <section className="hidden sm:flex justify-between sm:mt-12 md:mt-16 lg:mt-20 xl:mt-24" aria-labelledby="about-description">
+                <section className="hidden landscape:sm:flex justify-between sm:mt-12 md:mt-16 lg:mt-20 xl:mt-24" aria-labelledby="about-description">
                     <div className="w-2/5 ml-8">
-                        <h2 id="about-description" className="sr-only">
-                            About description
-                        </h2>
-                        <p className="font-arial text-[#DFDFDF] mr-3 sm:text-base md:text-lg lg:text-[22px] xl:text-2xl 2xl:text-3xl text-[32px] leading-snug">
-                            I'm fueled by my desire to create lasting solutions that go beyond an immediate fix. This has allowed me to balance business expectations and complex technological requirements to create practical and impactful solutions.
-                            I enjoy building end-to-end full-stack solutions, from database design and backend APIs to intuitive front-end user experiences.
-                            <br/>
-                            <br/>
-                            When I am not developing, you can find me admiring architecture, watching sports, or seeing the world through a viewfinder.
-                        </p>
+                        <AboutDesc/>
                     </div>
                     <div className="sm:w-[55%] overflow-hidden items-end">
                         <img
@@ -39,7 +31,7 @@ export default function About() {
                 </section>
 
                 {/* mobile */}
-                <section className="sm:hidden">
+                <section className="sm:hidden" aria-labelledby="about-description">
                     <div className="max-w-96 mx-10 mt-4 mb-10">
                         <p className="font-arial text-[#DFDFDF] text-2xl leading-snug">
                             I'm fueled by my desire to create lasting solutions that go beyond an immediate fix. This has allowed me to balance business expectations and complex technological requirements to create practical and impactful solutions.
@@ -52,6 +44,25 @@ export default function About() {
                     <div className="overflow-hidden">
                         <img
                             className="h-full object-cover mix-blend-luminosity"
+                            src="/images/chicago-skyline.jpg"
+                            alt="Chicago skyline at sunrise" 
+                        />
+                    </div>
+                </section>
+                {/* portrait */}
+                <section className="hidden sm:portrait:flex flex-col justify-between" aria-labelledby="about-description">
+                <div className="m-14 mt-8 lg:m-16 lg:mt-10 xl:m-20 xl:mt-14 2xl:m-24 2xl:mt-20">
+                        <p className="font-arial text-[#DFDFDF] text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl leading-snug">
+                            I'm fueled by my desire to create lasting solutions that go beyond an immediate fix. This has allowed me to balance business expectations and complex technological requirements to create practical and impactful solutions.
+                            I enjoy building end-to-end full-stack solutions, from database design and backend APIs to intuitive front-end user experiences.
+                            <br/>
+                            <br/>
+                            When I am not developing, you can find me admiring architecture, watching sports, or seeing the world through a viewfinder.
+                        </p>
+                    </div>
+                    <div className="overflow-hidden">
+                        <img
+                            className="h-full w-full object-cover mix-blend-luminosity"
                             src="/images/chicago-skyline.jpg"
                             alt="Chicago skyline at sunrise" 
                         />
