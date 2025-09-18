6d4fdc20edf4fc254dce052ce8cf06842130d72f
Sebastian Flores <flores7029@gmail.com>
2025-09-17 22:48:21 -0400

    Update ProjectCardTag.tsx

--- src/components/ProjectCardTag.tsx
+++ src/components/ProjectCardTag.tsx
@@ -6,7 +6,7 @@ export default function ProjectCardTag ({
     tech
 }) {
     return (
-        <div className="px-3 py-1 bg-[#D9D9D9] text-center shadow-[0px_4px_4px_rgba(0,0,0,0.15)] rounded-full
+        <div className="px-3 py-1 bg-[#D9D9D9] font-arial text-center shadow-[0px_4px_4px_rgba(0,0,0,0.15)] rounded-full
             text-md
             lg:text-lg
             xl:text-lg
